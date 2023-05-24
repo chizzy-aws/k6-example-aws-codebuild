f2e5daa7a3e0f3053148f7a591fd6ca92ddfb2d4
chizzy-aws <130452917+chizzy-aws@users.noreply.github.com>
2023-05-24 13:29:48 +0100

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-    const res = http.get('https://test.k6.io');
+    const res = http.get('http://cw-demo-env.eba-a4yhduwd.us-east-1.elasticbeanstalk.com/');
     sleep(1);
 }
