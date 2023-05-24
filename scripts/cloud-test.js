a141af18a64728501fcf725f7f4681f71e8e78b1
chizzy-aws <130452917+chizzy-aws@users.noreply.github.com>
2023-05-24 13:48:39 +0100

    Update cloud-test.js

--- scripts/cloud-test.js
+++ scripts/cloud-test.js
@@ -1,21 +1,43 @@
+// import http from 'k6/http';
+// import { sleep } from 'k6';
+
+
+// export const options = {
+//     ext: {
+//         loadimpact: {
+//             name: 'cloud-test',
+//         },
+//     },
+//     duration: '1m',
+//     vus: 50,
+//     thresholds: {
+//         http_req_duration: ['p(95)<500'],
+//     },
+// };
+
+// export default function () {
+//     const res = http.get('http://cw-demo-env.eba-a4yhduwd.us-east-1.elasticbeanstalk.com/');
+//     sleep(1);
+// }
+
+
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-
 export const options = {
-    ext: {
-        loadimpact: {
-            name: 'cloud-test',
-        },
+  ext: {
+    loadimpact: {
+      name: 'cloud-test',
     },
-    duration: '1m',
-    vus: 50,
-    thresholds: {
-        http_req_duration: ['p(95)<500'],
-    },
+  },
+  duration: '1m',
+  vus: 50,
+  thresholds: {
+    http_req_duration: ['p(95)<500'],
+  },
 };
 
 export default function () {
-    const res = http.get('http://cw-demo-env.eba-a4yhduwd.us-east-1.elasticbeanstalk.com/');
-    sleep(1);
+  const res = http.get('https://test.k6.io');
+  sleep(1);
 }
